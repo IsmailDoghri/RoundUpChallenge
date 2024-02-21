b42b2843381321333d45c727a1735da61a43791c
Ismail Doghri <ismail.doghri@kcl.ac.uk>
2024-02-20 23:08:18 -0500

    Adapt round-up function to spending category

--- main/utils.py
+++ main/utils.py
@@ -2,7 +2,7 @@ def calculate_round_up(transactions):
     """Calculate the total round-up amount for outgoing transactions."""
     total_round_up = 0
     for transaction in transactions:
-        if transaction["direction"] == "OUT":
+        if transaction["spendingCategory"] == "PAYMENTS":
             amount = transaction["amount"]["minorUnits"]
             round_up = (100 - (amount % 100)) % 100
             total_round_up += round_up
